4b7a02437f3acae8deedee213b8109e504271380
HOTTA Tatsuya <hotta.tatsuya.fcc@gmail.com>
2021-06-21 16:55:11 +0900

    marked

---
+++ pages/new.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col class="writeCol">
+        <b-textarea class="area" v-model="inputtedTxt" />
+      </b-col>
+      <b-col class="writeCol">
+        <div class="area" v-html="outputHTML" />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import sanitizeHTML from 'sanitize-html'
+import marked from 'marked'
+
+export default {
+  data() {
+    return {
+      inputtedTxt: '',
+    }
+  },
+  computed: {
+    outputHTML() {
+      const sanitizedTxt = sanitizeHTML(this.inputtedTxt)
+      const markedHTML = marked(sanitizedTxt)
+      return markedHTML
+    },
+  },
+}
+</script>
+
+<style scoped>
+.writeCol {
+  height: 100vh;
+}
+
+.area {
+  height: 100vh;
+}
+</style>
